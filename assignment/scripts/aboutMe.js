5f34e8f423f837e8d9acac18b640bfcb48ad27ef
tyguymills <111940838+tyguymills@users.noreply.github.com>
2022-11-15 09:43:37 -0600

    Update aboutMe.js

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,51 +1,80 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-
+let firstName = 'Tyler';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let fullName = "Miller";
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-
+let fullName = "tylerMiller";
 // 4 - Console log the value of `fullName`
-
+console.log('fullName');
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-
-// 6 - Console log this sentence, adding in the variables you created above: 
+let luckyNumber = 626;
+// 6 - Console log this sentence, adding in the variables you created above:
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
+console.log("My name is " + fullName + ", and I think " + 626 + " is a winner!")
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+let food = "Cheeseburger";
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let pets = 2;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 1;
 // 11 - Add two pets to your `pets` variable
-
+// let pet1 = "Trinnie";
+// let pet2 = "Chloe";
+// let pets =
+let pets = pets + 2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
-// 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
+const allowedPets = 5;
+// 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
-
+if(adventurous == true) {
+  console.log("How about we stay home?");
+} else {
+  console.log("How about we stay home?")
+}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
 
-// 15 - Write a conditional that console logs "I can have more pets!" 
+if(luckyNumber == 2 || adventurous == true) {
+  console.log("Roll the dice");
+}
+
+// 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+
+if(pets < allowedPets) {
+  console.log('I have enough pets');
+} else {
+  console.log("Oh no, I have too many pets!");
+}
 
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
 // correctly checks the `pets` and `friendsPets` variables, and
 // assigns the highest value to `mostPets`. There's several possibilities --
-// be sure to think through all the scenarios. 
+// be sure to think through all the scenarios.
 // console.log `mostPets` after the conditional has run.
+
+let mostPets;
+if(mostPets === friendsPets) {
+  console.log("mostPets and friendsPets are equal");
+}
+if(mostPets > friendsPets) {
+  console.log("mostPets");
+}
+if(mostPets < friendsPets) {
+  console.log("friendsPets");
+}
 
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
@@ -54,6 +83,36 @@
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 
+switch(luckyNumber) {
+  case 0:
+    luckyNumber == 1;
+    console.log("First is the worst");
+  case 1:
+    luckyNumber == 2;
+    console.log("Second is the best");
+  case 2:
+    luckyNumber == 3;
+    console.log("Third is the one with the polka dot dress");
+} else {
+  console.log("Luck is what happens when preparation meets opportunity");
+}
+
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
+// found this on mozilla mdn web docs. not sure what it does
 
+function adventurous(x) {
+  return (x ? adventurous == true);
+}
+console.log(adventurous(true));
+
+console.log(adventurous(false));
+
+console.log(adventurous(null));
+
+
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
